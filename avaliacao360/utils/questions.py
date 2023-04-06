4d24f39ceff660f696324458255f6d7e996c8468
danielvdbarbosa <danielvdbarbosa@gmail.com>
2023-04-06 20:04:06 -0300

    feat-criar-questionario-capturando-resultados

--- avaliacao360/utils/questions.py
+++ avaliacao360/utils/questions.py
@@ -1,7 +1,7 @@
 import PySimpleGUI as sg
 
 
-list_classmates = ['João', 'Maria', 'Clara', 'Ana', 'Claudio', 'Leandro']
+list_students = ['João', 'Maria', 'Clara', 'Ana', 'Claudio', 'Leandro']
 
 list_questions = [
     'se comunica de forma clara e objetiva.',
@@ -14,21 +14,38 @@ list_questions = [
 
 list_answers = []
 
-
-for question in list_questions:
-    for classmate in list_classmates:
-        layout = [
-            [sg.Text(f'{classmate} {question}')],
-            [
-                sg.Button('Discordo totalmente',
-                          size=(10, 5)),
-                sg.Button('Discordo parcialmente',
-                          size=(10, 5)),
-                sg.Button('Neutro',  size=(10, 5)),
-                sg.Button('Concordo parcialmente',  size=(10, 5)),
-                sg.Button('Concordo totalmente',  size=(10, 5)),
+while True:
+    for student in list_students:
+        for question in list_questions:
+            layout = [
+                [sg.Text(f'{student} {question}')],
+                [
+                    sg.Button('Discordo totalmente',
+                            s=(10, 5)),
+                    sg.Button('Discordo parcialmente',
+                            s=(10, 5)),
+                    sg.Button('Neutro',  s=(10, 5)),
+                    sg.Button('Concordo parcialmente',  s=(10, 5)),
+                    sg.Button('Concordo totalmente',  s=(10, 5)),
+                ]
             ]
-        ]
-        window = sg.Window('Questionário', layout)
-        event = window.read()
-        
+            window = sg.Window('Questionário', layout)
+            event, values = window.read()
+            print(event,values)
+
+            if event == 'Discordo totalmente':
+                list_answers.append(1)
+            elif event == 'Discordo parcialmente':
+                list_answers.append(2)
+            elif event == 'Neutro':
+                list_answers.append(3)
+            elif event == 'Concordo parcialmente':
+                list_answers.append(4)
+            elif event =='Concordo totalmente':
+                list_answers.append(5)
+    
+        print(f'Notas do aluno {student}: {list_answers}')
+        list_answers = []
+    break
+    
+    
