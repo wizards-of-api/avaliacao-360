d8ccf5a1490e071aab70dc227f3072a9de083e33
JulioFreitas07 <freitas27j@gmail.com>
2023-04-09 20:14:46 -0300

    Incluir botões da interface do administrador

--- avaliacao360/interface/adm.py
+++ avaliacao360/interface/adm.py
@@ -2,13 +2,23 @@ import app
 import PySimpleGUI as sg
 import interface.login as interface_login  
 
-def create_window():
-    layout = [[sg.Text('Administrador')], [sg.Button('Back'), sg.Button('Cancel')]]
-    return sg.Window('Window Title', layout)
+#Create Windows and layout
 
-def event_handler(event, _):
-  if event == 'Cancel':
+def create_window():
+  sg.theme('DarkAmber')
+  layout = [ 
+            [sg.Text('Administrador')], 
+            [sg.Button('Criar turma/grupo/aluno', key='create class', s=(18, 1) )],
+            [sg.Button('Resultados da avaliação', key='result evaluation', s=(18, 1))],
+            [sg.Button('Requisitar avaliação', key='request evaluation', s=(18, 1))],
+            [sg.Button('Lista de turmas/grupos', key='class list', s=(18, 1))],
+              ]
+  return sg.Window('Administrador', layout=layout, element_justification='c', finalize=True)
+  
+
+def event_handler(event, values):
+  if event == 'Cancel': # if user closes window or clicks cancel
     app.close()
   elif event == 'Back':
-    app.change_interface(interface_login.create_window(), interface_login.event_handler)
+    app.change_interface(interface_login)
 
