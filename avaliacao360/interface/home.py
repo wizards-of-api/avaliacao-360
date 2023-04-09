06b3808d814727e128f7f70183212822faca75aa
Matheus Marcy <mm.leitea@outlook.com>
2023-04-09 19:50:18 -0300

    :bricks: mudar estrutura do app
    change_interface agora recebe uma janela nova e um callback do event_handler

--- avaliacao360/interface/home.py
+++ avaliacao360/interface/home.py
@@ -2,17 +2,13 @@ import app
 import PySimpleGUI as sg
 import interface.login as interface_login  
 
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [[sg.Text('Bem-vindo!')],
-        [sg.Button('Login'), sg.Button('Cancel')],
-        ]
+def create_window():
+    layout = [[sg.Text('Bem-vindo!')],
+        [sg.Button('Login'), sg.Button('Cancel')]]
+    return sg.Window('Window Title', layout)
 
-# Create the Window
-window = sg.Window('Window Title', layout)
-
-def event_handler(event, values):
-    if event == 'Cancel': # if user closes window or clicks cancel
+def event_handler(event, _):
+    if event == 'Cancel':
         app.close()
     elif event == 'Login':
-        app.change_interface(interface_login)
+        app.change_interface(interface_login.create_window(), interface_login.event_handler)
--- avaliacao360/interface/student.py
+++ avaliacao360/interface/student.py
@@ -1,14 +1,13 @@
 import app
 import PySimpleGUI as sg
-import interface.login as interface_login  
-sg.theme('DarkAmber')   # Add a touch of color
-# All the stuff inside your window.
-layout = [ [sg.Text('Aluno')], [sg.Button('Back'), sg.Button('Cancel')]]
-# Create the Window
-window = sg.Window('Avaliação360', layout)
+import interface.login as interface_login
 
-def event_handler(event, values):
+def create_window():
+    layout = [ [sg.Text('Aluno')], [sg.Button('Back'), sg.Button('Cancel')]]
+    return sg.Window('Avaliação360', layout)
+
+def event_handler(event, _):
     if event == 'Cancel': # if user closes window or clicks cancel
         app.close()
     elif event == 'Back':
-        app.change_interface(interface_login)
+        app.change_interface(interface_login.create_window(), interface_login.event_handler)
