a688c25abb03ec4a4bdfdd79c6a8484640ba138e
Matheus Marcy <mm.leitea@outlook.com>
2023-04-09 19:50:18 -0300

    :bricks: mudar estrutura do app
    change_interface agora recebe uma janela nova e um callback do event_handler

--- avaliacao360/interface/student.py
+++ avaliacao360/interface/student.py
@@ -1,16 +1,18 @@
 import app
 import PySimpleGUI as sg
-import interface.login as interface_login  
-sg.theme('DarkAmber')
+import interface.login as interface_login
 
-col1 = [[sg.Text('Aluno: test')], [sg.Text('Turma: test')], [sg.Text('Grupo: test')], [sg.Text('')]]
+def create_window():
+    col1 = [[sg.Text('Aluno: test')], [sg.Text('Turma: test')], [sg.Text('Grupo: test')], [sg.Text('')]]
 
-layout = [[sg.Column(col1, justification='center', pad=(0, 10))], [sg.Button('Back'), sg.Button('Rate'), sg.Button('Cancel'), sg.Button('Result')]]
+    layout = [[sg.Column(col1, justification='center', pad=(0, 10))], [sg.Button('Back'), sg.Button('Rate'), sg.Button('Cancel'), sg.Button('Result')]]
 
-window = sg.Window('Avaliação360', layout, size=(230, 180))
+    window = sg.Window('Avaliação360', layout, size=(230, 180))
 
-def event_handler(event):
-    if event == 'Cancel':
+    return window
+
+def event_handler(event, _):
+    if event == 'Cancel': # if user closes window or clicks cancel
         app.close()
     elif event == 'Back':
-        app.change_interface(interface_login)
+        app.change_interface(interface_login.create_window(), interface_login.event_handler)
