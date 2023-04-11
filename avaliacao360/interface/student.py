6af3b16b432733cf4b584d6bd8c296c7b8d25ecd
Otavio Calderan <otaviocalderanadm@gmail.com>
2023-04-10 22:33:39 -0300

    corrigir texto

--- avaliacao360/interface/student.py
+++ avaliacao360/interface/student.py
@@ -3,16 +3,16 @@ import PySimpleGUI as sg
 import interface.login as interface_login
 
 def create_window():
-    col1 = [[sg.Text('Aluno: test')], [sg.Text('Turma: test')], [sg.Text('Grupo: test')], [sg.Text('')]]
+    col1 = [[sg.Text('Aluno: test')], [sg.Text('Turma: test')], [sg.Text('Grupo: test')], [sg.Text('\n')]]
 
-    layout = [[sg.Column(col1, justification='center', pad=(0, 10))], [sg.Button('Back'), sg.Button('Rate'), sg.Button('Cancel'), sg.Button('Result')]]
+    layout = [[sg.Column(col1, justification='center', pad=(0, 10))], [sg.Button('Voltar'), sg.Button('Avaliação'), sg.Button('Cancelar'), sg.Button('Resultados')]]
 
-    window = sg.Window('Avaliação360', layout, size=(230, 180))
+    window = sg.Window('Avaliação360', layout, size=(300, 180))
 
     return window
 
 def event_handler(event, _):
-    if event == 'Cancel':
+    if event == 'Cancelar':
         app.close()
-    elif event == 'Back':
+    elif event == 'Voltar':
         app.change_interface(interface_login.create_window(), interface_login.event_handler)
