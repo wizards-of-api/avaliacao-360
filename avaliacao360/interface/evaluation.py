1a1ea6be3519e26e626e1f5113314241dc803662
Matheus Marcy <mm.leitea@outlook.com>
2023-04-12 14:33:14 -0300

    :recycle: Matheus: Refatorar como radios são contruidos e lidos

--- avaliacao360/interface/evaluation.py
+++ avaliacao360/interface/evaluation.py
@@ -15,24 +15,31 @@ question_list = [
 def create_evaluation(student_list):
     evaluation = {}
     evaluated_index = 0 
-    
+
+    def run_trough_questions(callback):
+        for index, question in enumerate(question_list):
+            group_id = 'answer_' + str(index)
+            callback(group_id, question)
+
     def event_handler(event, values):
         nonlocal evaluation, evaluated_index
         if event == 'Enviar':
             result = []
             #para cada tela capturar a combinacao de resultados
             #iterar um contador para nomear o botao de radio capturado
-            for counter in range(len(question_list)):
-                if values['1'+'Answer'+ str(counter)]:
+            def callback(group_id, _):
+                if values['1_' + group_id]:
                     result.append(1)
-                if values['2'+'Answer'+ str(counter)]:
+                if values['2_' + group_id]:
                     result.append(2)
-                if values['3'+'Answer'+ str(counter)]:
+                if values['3_' + group_id]:
                     result.append(3)
-                if values['4'+'Answer'+ str(counter)]:
+                if values['4_' + group_id]:
                     result.append(4)
-                if values['5'+'Answer'+ str(counter)]:
+                if values['5_' + group_id]:
                     result.append(5)
+
+            run_trough_questions(callback)
 
             evaluation[student_list[evaluated_index]['name']] = result
 
@@ -44,30 +51,27 @@ def create_evaluation(student_list):
                 app.close()
                 
 
-    def create_question_layout(question_list):
+    def create_question_layout():
         column_list = []
-        counter = 0
 
-        for question in question_list:
-    
-            group_id = 'Answer' + str(counter)
-            
+        def callback(group_id, question):
             column_list.append([sg.Text(student_list[evaluated_index]['name'] +' '+ question)])
             column_list.append(
-                [sg.Radio('Discordo totalmente', group_id, key = '1' + str(group_id)),
-                sg.Radio('Discordo', group_id, key = '2' + str(group_id)),
-                sg.Radio('Neutro', group_id, key = '3' + str(group_id)),
-                sg.Radio('Concordo', group_id, key = '4' + str(group_id)), 
-                sg.Radio('Concordo totalmente', group_id, key = '5' + str(group_id))]
+                [sg.Radio('Discordo totalmente', group_id, key = '1_' + str(group_id)),
+                sg.Radio('Discordo', group_id, key = '2_' + str(group_id)),
+                sg.Radio('Neutro', group_id, key = '3_' + str(group_id)),
+                sg.Radio('Concordo', group_id, key = '4_' + str(group_id)), 
+                sg.Radio('Concordo totalmente', group_id, key = '5_' + str(group_id))]
             )
-            counter += 1
+
+        run_trough_questions(callback)
 
         column_list.append([sg.Button('Enviar', size = (6,0))])
 
         return column_list
 
     def evaluate():
-        layout = create_question_layout(question_list)
+        layout = create_question_layout()
         window = sg.Window('Questionário' + student_list[0]['name'], layout, element_justification = 'c')
         app.change_interface(window, event_handler)
 
