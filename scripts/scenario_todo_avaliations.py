b8fb829e725f1bb24f077e02d440c036f80778c4
Matheus Marcy <mm.leitea@outlook.com>
2023-04-12 12:31:48 -0300

    Matheus: Remover prints e alguns espaços entre linhas

--- scripts/scenario_todo_avaliations.py
+++ scripts/scenario_todo_avaliations.py
@@ -1,9 +1,9 @@
 from reset_mock_db import reset
 import avaliacao360.connection.evaluation as evaluation_connection
 
-evaluation_connection.answer_evaluation
 reset()
+
 evaluation_connection.request_evaluation()
-print(evaluation_connection.resolve_evaluation_by_id(1))
+
 evaluation_connection.answer_evaluation(1, { 1: [2, 5, 3, 3, 3, 4], 2: [1, 1, 1, 1, 1, 1] })
 evaluation_connection.answer_evaluation(2, { 1: [2, 5, 3, 3, 3, 4], 2: [1, 1, 1, 1, 1, 1] })
