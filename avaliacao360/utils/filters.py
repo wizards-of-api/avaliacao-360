6ef1052dd78ce6d68ecd7700081a0e389a54c65a
Matheus Marcy <mm.leitea@outlook.com>
2023-04-12 12:20:32 -0300

    Matheus: Adicionar replace_on_list

--- avaliacao360/utils/filters.py
+++ avaliacao360/utils/filters.py
@@ -1,2 +1,8 @@
 def filter_by_key(list, key_name, key_value):
     return [dict for dict in list if dict[key_name] == key_value][0]
+
+def replace_on_list(list, filter_key, new_value):
+    index = [index for index, dict in enumerate(list) if dict[filter_key] == new_value[filter_key]][0]
+    list[index] = new_value
+    
+    return list
