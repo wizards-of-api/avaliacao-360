086e0839293734e2592f1bb9bff732e656161a9d
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2023-04-13 14:03:29 -0300

    mostrar dados do aluno

--- avaliacao360/interface/login.py
+++ avaliacao360/interface/login.py
@@ -2,25 +2,27 @@ import app
 import PySimpleGUI as sg
 import interface.student as interface_student 
 import interface.adm as interface_adm
+import connection.student as connection_student
 
 def create_window():
     layout = [[sg.Text('Login!')],
-        [sg.Text('Usuário: ', size = (6,0)), sg.InputText(size = (20,0), key='input')],
-        [sg.Text('Senha: ', size = (6,0)), sg.InputText(size = (20,0))],
-        [sg.Text('\n')],
-        [sg.Button('Login'), sg.Button('Cancelar')]
+        [sg.Text('User: '), sg.InputText(key='input')],
+        [sg.Text('Password: '), sg.InputText()],
+        [sg.Button('Login'), sg.Button('Cancel')]
         ]
 
-    return sg.Window('Avaliação 360 - Login', layout, element_justification = 'c')
+    return sg.Window('Window Title', layout)
 
 def event_handler(event, values):
-    if event == 'Cancelar':
+    input_name = values['input']
+    student_list = connection_student.get_student_by_name(input_name)
+
+    if event == 'Cancel':
         app.close()
     elif event == 'Login':
-        input_text_value = values['input']
-        if input_text_value =='aluno':
-            app.change_interface(interface_student.create_window(), interface_student.event_handler)
-        elif input_text_value =='adm':
-            app.change_interface(interface_adm.create_window(), interface_adm.event_handler)
+        if student_list:
+            app.change_interface(interface_student.create_window(input_name), interface_student.event_handler)
+        elif input_name =='adm':
+            app.change_interface(interface_adm.create_window(), interface_adm.event_handler, input_name  )
         else: 
             app.pop_up('Não possui Login')
