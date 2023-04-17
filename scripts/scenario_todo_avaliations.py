9d318c3aaf0144731cce1b4b681de674a5395f6b
Matheus Marcy <mm.leitea@outlook.com>
2023-04-17 16:00:59 -0300

    :art: Matheus: alterar valores salvos no script

--- scripts/scenario_todo_avaliations.py
+++ scripts/scenario_todo_avaliations.py
@@ -12,6 +12,6 @@ joao_id = student_connection.get_student_by_name('João')[0]['id']
 maria_eval_id = student_connection.get_student_evaluation_by_id(maria_id)[0]['id']
 joao_eval_id = student_connection.get_student_evaluation_by_id(joao_id)[0]['id']
 
-evaluation_connection.answer_evaluation(1, maria_eval_id, { 1: [2, 5, 3, 3, 3, 4], 2: [1, 1, 1, 1, 1, 1] })
-evaluation_connection.answer_evaluation(2, joao_eval_id, { 1: [2, 5, 3, 3, 3, 4], 2: [1, 1, 1, 1, 1, 1] })
+evaluation_connection.answer_evaluation(1, maria_eval_id, { 1: [2, 5, 3, 3, 3, 4], 2: [3, 5, 4, 4, 1, 2] })
+evaluation_connection.answer_evaluation(2, joao_eval_id, { 1: [5, 2, 2, 2, 3, 1], 2: [4, 4, 2, 5, 4, 2] })
 
