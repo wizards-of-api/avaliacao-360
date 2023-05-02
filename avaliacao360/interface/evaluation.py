6e9925274edce8d7f74aa1ee576a9ca8d07182b0
danielvdbarbosa <danielvdbarbosa@gmail.com>
2023-05-02 15:41:47 -0300

    documentar as funções do arquivo

--- avaliacao360/interface/evaluation.py
+++ avaliacao360/interface/evaluation.py
@@ -2,7 +2,7 @@ import app
 import PySimpleGUI as sg
 import interface.login as interface_login
 from connection.evaluation import answer_evaluation
-from connection.student import get_student_evaluation_by_id, get_student_by_id
+from connection.student import get_student_by_id
 from config import question_list
 
 def create_evaluation(student_id, evaluation_id, student_list):
@@ -10,17 +10,25 @@ def create_evaluation(student_id, evaluation_id, student_list):
     evaluated_index = 0
     
     def run_trough_questions(callback):
+        """Cria um indice dinamico para os grupos de resposta de cada
+        pergunta."""
+
         for index, question in enumerate(question_list):
             group_id = 'answer_' + str(index)
             callback(group_id, question)
 
     def event_handler(event, values):
+        """Recebe os eventos e valores capturados na tela quando o botão
+        Enviar é apertado. Se o valor de algum radio for 2 ou 1, captura
+        um comentário de justificativa."""
+
         nonlocal evaluation, evaluated_index
         if event == 'Enviar':
             result = []
             
             def callback(group_id, _):
-                if values['1_' + group_id]:
+                """"""
+                if values['1_' + group_id]:#and evaluated_index != student_id:
                     feedback = None
                     while not feedback or feedback == "":
                         feedback = sg.popup_get_text(
@@ -28,7 +36,7 @@ def create_evaluation(student_id, evaluation_id, student_list):
                             title='Justificativa'
                             )
                     result.append({'value':1, 'feedback':feedback})
-                if values['2_' + group_id]:
+                if values['2_' + group_id]:#and evaluated_index != student_id:
                     feedback = None
                     while not feedback or feedback == "":
                         feedback = sg.popup_get_text(
@@ -43,7 +51,7 @@ def create_evaluation(student_id, evaluation_id, student_list):
                 if values['5_' + group_id]:
                     result.append({'value':5, 'feedback':None})
 
-            #mock dos estudantes
+            
             def mock_students():
                 answer_evaluation(student_id, evaluation_id, evaluation)
 
@@ -59,9 +67,15 @@ def create_evaluation(student_id, evaluation_id, student_list):
                 app.change_interface(interface_login.create_window(), interface_login.event_handler)
 
     def create_question_layout():
+        """Cria a interface, utilizando as funções callback e 
+        run_through_questions."""
+
         column_list = []
 
         def callback(group_id, question):
+            """Cria as perguntas e os botões de resposta usando o indice dinamico
+            criado anteriormente."""
+
             column_list.append([sg.Text(student_list[evaluated_index]['name'] +' '+ question)])
             column_list.append(
                 [sg.Radio('Discordo totalmente', group_id, key = '1_' + str(group_id)),
@@ -80,6 +94,9 @@ def create_evaluation(student_id, evaluation_id, student_list):
         return column_list
 
     def evaluate():
+        """Inicializa o layout da avaliação e setta a 
+        janela para capturar eventos com o arquivo app. """
+
         layout = create_question_layout()
         window = sg.Window('Avaliação 360 - Questionário ' + get_student_by_id(student_id)['name'], layout, element_justification = 'c')
         app.change_interface(window, event_handler)
