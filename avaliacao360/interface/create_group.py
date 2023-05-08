350e501724e877e37835712d1e481ef93f66eb12
Otavio Calderan <otaviocalderanadm@gmail.com>
2023-05-08 11:41:35 -0300

    Revert "Corrigir erro de input dentro da caixa de lista de turmas"
    This reverts commit a2aa814100f584630bf52638b5e53e72edd8e4e4.

--- avaliacao360/interface/create_group.py
+++ avaliacao360/interface/create_group.py
@@ -7,10 +7,9 @@ from utils.filters import filter_by_key
 
 def create_window():
     class_room_list = connection_class_room.get_class_room_list()
-    class_room_names = [class_room['name'] for class_room in class_room_list]
     layout = [
         [sg.Text('Nome do Grupo'), sg.Input(key='input')],
-        [sg.Text('Turma'), sg.Combo(class_room_names, readonly=True, key='list')],
+        [sg.Text('Turma'), sg.Combo(class_room_list, key='list')],
         [sg.Button('Registrar Grupo', key='create group')],
         [sg.Button('Voltar', key='return interface')]
         ]
@@ -20,8 +19,7 @@ def create_window():
 def event_handler(event, values):
     group_list = connection_group.get_group_list()
     input_groupname = values['input']
-    input_class_room_name = values['list']
-
+    input_class_room = values['list']
     if event == 'return interface':
         app.change_interface(create_room.create_window(), create_room.event_handler)
     elif event == 'create group':
@@ -29,14 +27,8 @@ def event_handler(event, values):
             sg.popup('Por favor, preencha o nome do grupo')
         elif filter_by_key(group_list, 'name', input_groupname):
             sg.popup('Nome de grupo já existente')
-        elif input_class_room_name == '':    
+        elif input_class_room == '':
             sg.popup('Por favor, selecione uma turma')
         else:
-            class_room_list = connection_class_room.get_class_room_list()
-            class_room = filter_by_key(class_room_list, 'name', input_class_room_name)
-            if class_room:
-                class_room_id = class_room[0]['id']
-                connection_group.create_group({'name': input_groupname, 'class-room-id': class_room_id})
-                sg.popup(f'Grupo {input_groupname} criado com sucesso!')
-            else:
-                sg.popup('Turma não encontrada')
+            connection_group.create_group({'name': input_groupname, 'class-room-id':input_class_room['id']})
+            sg.popup(f'Grupo {input_groupname} criado com sucesso!')
