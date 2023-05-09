0fe35b8d1b31d7ae08d32bb678980330ab631e8a
brunodos-santos <bruno6.santos@usp.br>
2023-05-09 01:38:40 -0300

    Bruno: Inserção da funcionalidade do adm de cadastro de novos usuários. Ainda falta fazer o login ler do json com os cadastros

---
+++ avaliacao360/interface/create_registration.py
@@ -0,0 +1,63 @@
+import PySimpleGUI as sg
+import interface.create_room as create_room
+import app
+import json
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', 'utils')))
+
+
+def create_window(key):
+    layout = [
+        [sg.Text('Nome do Aluno:', size=(14,1)), sg.Input('', key='name', size=(20,1))],
+        [sg.Text('Senha:', size=(14,1)), sg.Input('', key='password', password_char='*', size=(20,1))],
+        [sg.Text('', key='error', text_color='red')],
+        [sg.Text('', key='output')],
+        [sg.Button('Cadastrar', key='register', size=(10,1)),
+         sg.Button('Voltar', key='return', size=(10,1), button_color=('white', 'gray'))]
+    ]
+    return sg.Window('Cadastro de Aluno', layout, element_justification='c', finalize=True)
+
+
+
+def event_handler(event, values):
+    print('Evento:', event)  # adicionando um print para verificar o evento
+    if event == 'return':
+        app.change_interface(create_room.create_window(),
+                             create_room.event_handler)
+    elif event == 'register':
+        name = values['name']
+        password = values['password']
+
+        # Validar se o nome e senha foram preenchidos
+        if not name.strip() or not password.strip():
+            app.pop_up('Por favor, preencha o nome e a senha')
+            return
+
+        # Validar se a senha tem pelo menos 8 caracteres
+        if len(password) < 8:
+            app.pop_up('A senha deve ter pelo menos 8 caracteres')
+            return
+
+        # Armazenar o cadastro em um arquivo JSON
+         
+        path = os.path.join(os.path.dirname(__file__), '..', 'utils', 'users_register.json')
+        if os.path.exists(path) and os.path.getsize(path) > 0:
+            with open(path, 'r') as f:
+                data = json.load(f)
+        else:
+            data = {}
+
+        if name in data:
+            app.pop_up('Este nome de usuário já foi cadastrado')
+            return
+
+        data[name] = password
+
+        with open(path, 'w') as f:
+            json.dump(data, f)
+
+        app.pop_up('Cadastro realizado com sucesso')
+
--- avaliacao360/interface/create_room.py
+++ avaliacao360/interface/create_room.py
@@ -1,20 +1,28 @@
 import PySimpleGUI as sg
 import interface.adm as interface_adm
 import interface.create_class as create_class
+import interface.create_registration as create_registration
 import app
+
 
 def create_window():
     layout = [
         [sg.Text('O que deseja criar?')],
-        [sg.Button('Turma', key= 'class', s=(18,1))],
-        [sg.Button('Grupo', key= 'group', s=(18,1))],
-        [sg.Button('Aluno', key= 'student', s=(18,1))],
+        [sg.Button('Turma', key='class', s=(18, 1))],
+        [sg.Button('Grupo', key='group', s=(18, 1))],
+        [sg.Button('Aluno', key='student', s=(18, 1))],
         [sg.Button('Voltar', key='return interface', s=(18, 1))]
-        ]
-    return sg.Window('Avaliação 360 - Criar Classe/Turma/Grupo', layout, element_justification='c', finalize= True)
+    ]
+    return sg.Window('Avaliação 360 - Criar Classe/Turma/Grupo', layout, element_justification='c', finalize=True)
+
 
 def event_handler(event, _):
     if event == 'return interface':
-        app.change_interface(interface_adm.create_window(), interface_adm.event_handler)
+        app.change_interface(interface_adm.create_window(),
+                             interface_adm.event_handler)
     elif event == 'class':
-        app.change_interface(create_class.create_window(key='output'), create_class.event_handler)
+        app.change_interface(create_class.create_window(
+            key='output'), create_class.event_handler)
+    elif event == 'student':
+        app.change_interface(create_registration.create_window(
+            key='output'), create_registration.event_handler)
