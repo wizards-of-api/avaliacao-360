c5052bbb8e4cbe4cef26d859401386ef23c8fe93
Matheus Marcy <mm.leitea@outlook.com>
2023-05-13 20:16:57 -0300

    :bug: arrumar script quebrando

--- scripts/reset_mock_db.py
+++ scripts/reset_mock_db.py
@@ -1,13 +1,13 @@
 import sys, os
 from datetime import datetime, timedelta
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'avaliacao360', 'connection')))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'avaliacao360', 'interface')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'avaliacao360')))
 
-import connection.controller as controller
-import interface.entity_manager as entity_manager
-import interface.group as group
-import interface.student as student
+from connection import controller
+from connection import class_room
+from connection import group
+from connection import student
+from connection import login
 
 date_format = '%d/%m/%Y'
 
@@ -15,16 +15,16 @@ def reset():
     controller.create_db()
     controller.clear_db()
 
-    room_a_id = entity_manager.create_entity_manager({'name': 'Sala A'})
-    room_b_id = entity_manager.create_entity_manager({'name': 'Sala B'})
+    room_a_id = class_room.create_class_room({'name': 'Sala A'})
+    room_b_id = class_room.create_class_room({'name': 'Sala B'})
 
     sprint_end = datetime.today() - timedelta(days=1)
     sprint_start = sprint_end - timedelta(days=15)
 
     sprint_object = {'start': sprint_start.strftime(date_format), 'end': sprint_end.strftime(date_format)}
 
-    entity_manager.add_sprint(room_a_id, sprint_object)
-    entity_manager.add_sprint(room_b_id, sprint_object)
+    class_room.add_sprint(room_a_id, sprint_object)
+    class_room.add_sprint(room_b_id, sprint_object)
 
     group_mega_id = group.create_group({'class-room-id': room_a_id, 'name': 'Grupo Mega'})
     group_ultra_id = group.create_group({'class-room-id': room_a_id, 'name': 'Grupo Ultra'})
@@ -33,16 +33,24 @@ def reset():
     group_super_id = group.create_group({'class-room-id': room_b_id, 'name': 'Grupo Super'})
 
     student.create_student({'group-id': group_mega_id, 'name': 'Maria'})
+    login.create_user({'username': 'maria', 'password': '12345678'})
     student.create_student({'group-id': group_mega_id, 'name': 'João'})
+    login.create_user({'username': 'joao', 'password': '12345678'})
 
     student.create_student({'group-id': group_ultra_id, 'name': 'Ana'})
+    login.create_user({'username': 'ana', 'password': '12345678'})
     student.create_student({'group-id': group_ultra_id, 'name': 'Gabriel'})
+    login.create_user({'username': 'gabriel', 'password': '12345678'})
 
     student.create_student({'group-id': group_blast_id, 'name': 'Roberta'})
+    login.create_user({'username': 'roberta', 'password': '12345678'})
     student.create_student({'group-id': group_blast_id, 'name': 'Lucas'})
+    login.create_user({'username': 'lucas', 'password': '12345678'})
 
     student.create_student({'group-id': group_super_id, 'name': 'Luana'})
+    login.create_user({'username': 'luana', 'password': '12345678'})
     student.create_student({'group-id': group_super_id, 'name': 'Davi'})
+    login.create_user({'username': 'davi', 'password': '12345678'})
 
 if __name__ == '__main__':
     reset()
