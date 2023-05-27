73f474be3f96fa8c70c5a5000e8c22a6345ae0e3
pauloarantesmachado <pauloantoniomachado@hotmail.com>
2023-05-27 13:22:48 -0300

    Dados para mostrar na tela de dash geral

--- avaliacao360/connection/student_dash.py
+++ avaliacao360/connection/student_dash.py
@@ -11,11 +11,9 @@ def evaluation_score(name, sprint, group):
     sprint_student = sprint
     id_student = ''
     group_id_student = group
-
     for student in json_db['student-list']:
       if student['name'] == name_student:
         id_student = str(student['id'])
-
     for evaluation in json_db['evaluation-list']:
       if evaluation['group-id'] == group_id_student and evaluation['sprint'] == sprint_student:
         dict_evaluation = evaluation['answer-dict']
@@ -54,34 +52,34 @@ def evaluation_score(name, sprint, group):
         }
         return evaluation_dict_student
       
-def general_dash(name):
+def general_dash(id_student, group_id):
   with open(mock_path, 'r') as mock_db_file:
     mock_db_str = mock_db_file.read()
     json_db = json.loads(mock_db_str)
-    name_student = name
-    group_id_list = []
+    id_student_data = id_student 
+    name_student = ''
+    group_id_data = group_id
     name_group = []
-    id_group = 0
     sprint = 0
     for student in json_db['student-list']:
-      if student['name'] == name:
+      if student['id'] == id_student_data:
+        name_student = student['name']
+        print(name_student)
         group_id_list = student['group-id-list']
         for group in group_id_list:
-          id_group = group
-          for names in json_db['group-list']:
-            if id_group == names['id']:
-              name_group.append(names['name'])
+          if group_id_data == group:
+            for names in json_db['group-list']:
+              if group_id_data == names['id']:
+                name_group = names['name']
     for sprints in json_db['evaluation-list']:
-      if sprints['group-id'] == id_group:
+      if sprints['group-id'] == group_id_data:
         sprint = sprints['sprint']
-    avarage = evaluation_score(name_student, sprint, id_group)
+    avarage = evaluation_score(name_student, sprint ,group_id)
     generate_dict_student = {
-      'values': avarage['values'],
       'average': avarage['average'],
       'group': name_group,
       'sprint': sprint,
     }
-    print(generate_dict_student)
     return generate_dict_student
 
 
@@ -99,19 +97,19 @@ def media_list(list):
   nota3 = 0
   nota4 = 0
   nota5 = 0
-  nota6 = 0
   for score in list:
     nota1 += score[0]
     nota2 += score[1]
     nota3 += score[2]
     nota4 += score[3]
     nota5 += score[4]
-    # nota6 += score[5]
+  if len(list > 0) :    
+    list_avaluation_average =[ nota1/len(list),
+    nota2/len(list), nota3/len(list), nota4/len(list),
+    nota5/len(list)]
+  else:
+    list_avaluation_average = [0]
     
-  list_avaluation_average =[ nota1/len(list),
-  nota2/len(list), nota3/len(list), nota4/len(list),
-  nota5/len(list)]
-
   return list_avaluation_average
   
 
